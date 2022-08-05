f2e131a1ffe26439b3c5380b4b4b2ed02e5b828d
Gavin Carew <gjcarew@gmail.com>
2022-08-04 20:54:18 -0700

    test: finish writing Enigma tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,29 +2,60 @@ require './lib/enigma'
 
 RSpec.describe Enigma do
   before :each do
-    @enigma = Enigma.new("hello world", "02715", "040895")
+    @enigma = Enigma.new
   end
 
   it 'exists' do
     expect(@enigma).to be_an Enigma
   end
 
-  it 'an attributes' do
-    expect(@enigma.message).to eq("message string")
-    expect(@enigma.key).to eq("02715")
-    expect(@enigma.date).to eq("040895")
+  it 'encrypts a message with a key and date' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
+    {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
   end
 
-  it 'has default attributes for key and date' do
-    enigma2 = Enigma.new("other message")
-    allow(enigma2).to receive(:rand_key).and_return("98116")
-    allow(enigma2).to receive(:today).and_return("04082022")
-    expect(enigma2.key).to eq("98116")
-    expect(enigma2.date).to eq("04082022")
+  it 'decrypts a message with a key and date' do
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(
+    {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    })
   end
 
+  it 'encrypts a message with just a key' do
+    allow(@enigma).to receive(:date_today).and_return("040895")
+    expect(@enigma.encrypt("hello world", "02715")).to eq(
+    {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
+  end
 
+  it 'decrypts a message with a key' do
+    allow(@enigma).to receive(:date_today).and_return("040895")
+    expect(@enigma.decrypt("keder ohulw", "02715")).to eq(
+    {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    })
+  end
 
-
+  it 'encrypts a message with a random key and todays date' do
+    allow(@enigma).to receive(:date_today).and_return("040895")
+    allow(@enigma).to receive(:rand_key).and_return("02715")
+    expect(@enigma.encrypt("hello world")).to eq(
+    {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
+  end
 
 end
