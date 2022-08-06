d30ddab8088b6521b279b813ee2b6ea4a970acf3
Gavin Carew <gjcarew@gmail.com>
2022-08-06 16:50:58 -0700

    test: add tests to decryption_spec

---
+++ lib/decryption.rb
@@ -0,0 +1,18 @@
+class Decryption
+
+  def decrypt
+
+  end
+
+  def decrypted_array(message, key, date)
+    decrypted_a = []
+    message.each_slice(4) do |a, b, c, d|
+      encrypted_a << scramble(key, date, :A, a)
+      encrypted_a << scramble(key, date, :B, b)
+      encrypted_a << scramble(key, date, :C, c)
+      encrypted_a << scramble(key, date, :D, d)
+    end
+    encrypted_a.compact
+  end
+
+end
