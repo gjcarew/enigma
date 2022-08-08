7c9fc81907f4c7be06f52e6907694b0e96d281fb
Gavin Carew <gjcarew@gmail.com>
2022-08-08 16:35:27 -0700

    Change input path to go through Validate

--- encrypt.rb
+++ encrypt.rb
@@ -1,2 +1,3 @@
 require_relative 'lib/input_output'
-InputOutput.new(ARGV, "encrypt")
+
+Validate.new.validate(ARGV, 'encrypt')
--- lib/validate.rb
+++ lib/validate.rb
@@ -1,1 +1,46 @@
-validate()
+class Validate
+
+  def validate(args, what_to_do)
+    if validate_filepath(args) &&
+      validate_num_args(args) &&
+      validate_key_date_numeric?(args) &&
+      validate_key_date_length(args)
+      InputOutput.new(args, what_to_do)
+    end
+  end
+
+  def validate_filepath(args)
+    if !args[0..1].all?{ |filepath| filepath[-4..-1] == ".txt"}
+      puts "Please make sure your filepaths are .txt files"
+      false
+    else
+      true
+    end
+  end
+
+  def validate_num_args(args)
+    if args.length > 4 || args.length < 2
+      puts "Wrong number of arguments"
+      false
+    else
+      true
+    end
+  end
+
+  def validate_key_date_numeric?(args)
+    if !args[2..3].all?{ |arg| arg != nil && !arg.match?(/\D/)}
+      puts "Please make sure your key and date are all numeric"
+      false
+    else
+      true
+    end
+  end
+
+  def validate_key_date_length(args)
+    if !args[2..3].all?{ |arg| arg != nil && [5, 6].include?(arg.length)}
+      puts "Please make sure your key is 5 digits and your date is formatted 'DDMMYY'"
+      false
+    else
+      true
+    end
+  end
